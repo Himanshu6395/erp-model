a429de97b8d44dff7462bb9d44b5fc3e1fcbd05e
Himanshu6395 <rajhimanshu122kumar@gmail.com>
2026-04-02 16:48:54 +0530

    new

--- backend/src/modules/user/controller.js
+++ backend/src/modules/user/controller.js
@@ -4,7 +4,7 @@ const createSchoolAdmin = async (req, res) => {
   const data = await userService.createSchoolAdmin(req.body);
   return res.status(201).json({ success: true, data });
 };
-
+console.log("fjnfg");
 export const userController = {
   createSchoolAdmin,
 };
