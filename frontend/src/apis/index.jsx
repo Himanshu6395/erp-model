0d4659cbc659dfc4f76a9d538df90478cf750420
Himanshu6395 <rajhimanshu122kumar@gmail.com>
2026-04-02 22:07:13 +0530

    d

--- frontend/src/apis/index.jsx
+++ frontend/src/apis/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import {store} from "../store/store.js";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://43.205.113.119:5000',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
